d0cd41db12381b83bb7c6616faa517e32fc45f1b
Sameer Kumar <sameerkr.dev@gmail.com>
2025-11-30 22:02:46 +0530

    setup winston and morgan for logging

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 import env from "@/config/dotenv";
+import logger from "./config/logger";
 
 const app = express();
 
+// Morgan logs HTTP requests
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
+  logger.info("Welcome to shridhan");
   res.json({ message: "Welcome to Shridhan" });
 });
 
 app.listen(env.PORT, () => {
-  console.log("Server starting on port 5801");
+  logger.info(`Server is running on PORT ${env.PORT}`);
 });
